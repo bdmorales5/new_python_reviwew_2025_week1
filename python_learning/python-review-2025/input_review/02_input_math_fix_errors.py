f0493605d25c5f2b5aee6770edaabd5813286c89
Morales <bdmorales5@cps.k12.il.us>
2025-10-24 13:33:37 -0500

    testing

--- python_learning/python-review-2025/input_review/02_input_math_fix_errors.py
+++ python_learning/python-review-2025/input_review/02_input_math_fix_errors.py
@@ -1,17 +1,41 @@
 """
 Fix the Errors #2 — Input & Math
 """
-x = int(input("Enter a number: "))
-y = int(input("Enter another number: "))
-print(f"Sum: {x + y}")
-age = input("How old are you?")
-print(f"In 10 years you will be {int(age)} + 10")
-n = float(input("Enter a numerator: "))
-d = float(input("Enter a denominator: "))
-result = n / d
-print(f"Result: {result}")
-a = 5; b = 2
-print(f"Power: {a ^ 2}")
-print(f"Remainder: {a // b}")
-pi = 3.1415926535
-print(f"Pi is approximately {pi}")
+# x = int(input("Enter a number: "))
+# y = int(input("Enter another number: "))
+# print(f"Sum: {x + y}")
+# age = input("How old are you?")
+# print(f"In 10 years you will be {int(age)} + 10")
+# n = float(input("Enter a numerator: "))
+# d = float(input("Enter a denominator: "))
+# result = n / d
+# print(f"Result: {result}")
+# a = 5; b = 2
+# print(f"Power: {a ^ 2}")
+# print(f"Remainder: {a // b}")
+# pi = 3.1415926535
+# print(f"Pi is approximately {pi}")
+
+# x = 1
+# y = 2
+# z = 3
+# result= (x+y)* z -(y/x)
+# print(f"result:{result}")
+# print(f"division:{y/x}")
+# print(f"addition:{x+y}")
+# print(f"multiplication:{(x+y)*z}")
+# print(f"subtraction: {(x+y)*z-(y/x)}")
+# print(f"modulo:{y%x}")
+# print(f"power:{x**y}")
+# print(f"absolute value:{abs(x-y)}")
+# print(f"max{max(x,y,z)}")
+# print(f"min:{min(x,y,z)}")
+# pi=3.1415926535
+# print(f"round:{round(pi)}")
+# from math import*
+# print(f"square root of 16:{round(16)}")
+# print(f"ceiling of pi:{ceil(pi)}")
+# print(f"floor of pi:{floor(pi)}")
+
+
+
